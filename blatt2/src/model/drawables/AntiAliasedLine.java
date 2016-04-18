6ff8970b9260c36edb94ac64f127777aa134900b
Tleuchs <tleuchs@uni-osnabrueck.de>
2016-04-18 23:50:10 +0200

    A2 unfinished

--- blatt2/src/model/drawables/AntiAliasedLine.java
+++ blatt2/src/model/drawables/AntiAliasedLine.java
@@ -39,24 +39,75 @@ public class AntiAliasedLine extends DrawableObject {
 		// TODO: (A2) AntiAlias-Bresenham mit Hilfe von `setPixel()` aus der
 		// Superklasse implementieren
 		g.setXORMode(Color.ORANGE);
-
+		int inc_x = 0;
+		int inc_y = 0;
 		int dy = e.y - a.y;
 		int dx = e.x - a.x;
-		int delta = 2 * dy;
-		int error = dx;
-		int schritt = -2 * dx;
 		int x = a.x;
 		int y = a.y;
-		while (x <= e.x) {
-			setPixel(x, y, g);
-			x++;
-			error = error + delta;
-			if (error > 0) {
-				y++;
-				error = error + schritt;
+
+		if (dx > 0) // Linie nach rechts?
+			inc_x = 1; // x inkrementieren
+		else // Linie nach links
+			inc_x = -1; // x dekrementieren
+
+		if (dy > 0) // Linie nach unten?
+			inc_y = 1; // y inkrementieren
+		else // Linie nach oben
+			inc_y = -1; // y dekrementieren
+
+		if (Math.abs(dy) < Math.abs(dx)) {
+			float error = -Math.abs(dx); // Fehler bestimmen
+			int delta = 2 * Math.abs(dx); // Delta bestimmen
+			float schritt = 2 * error; // Schwelle bestimmen
+			float fehler = Math.abs(error) / 100f;
+			if (fehler > 1) {
+				fehler = 1;
 			}
+			if (fehler < 0) {
+				fehler = 0;
+			}
+			float brightness = 1 - fehler;
+			while (x != e.x) {
+				setPixel(x, y, brightness, g);// setze Pixel
+				x += inc_x;
+				error = error + delta;
+				if (error > 0) {
+					y += inc_y;
+					error += schritt;
+				}
+				if (error > 0.5) {
+					setPixel(x, y - 1, fehler, g);// setze Error Pixel
+				}
+			}
+		} else { // steil nach oben oder unten
+			float error = -Math.abs(dy); // Fehler bestimmen
+			int delta = 2 * Math.abs(dx); // Delta bestimmen
+			float schritt = 2 * error; // Schwelle bestimmen
+			float fehler = Math.abs(error) / 100f;
+			if (fehler > 1) {
+				fehler = 1;
+			}
+			if (fehler < 0) {
+				fehler = 0;
+			}
+			float brightness = 1 - fehler;
+			while (y != e.y) { // fuer jede y-Koordinate
+				setPixel(x, y, brightness, g);// setze Pixel
+
+				y += inc_y; // naechste y-Koordinate
+				error = error + delta; // Fehler aktualisieren
+				if (error > 0) { // neue Zeile erreicht?
+					x += inc_x; // x-Koord. aktualisieren
+					error += schritt; // Fehler aktualisieren
+				}
+				if (error > 0.5) {
+					setPixel(x, y - 1, fehler, g);// setze Error Pixel
+				}
+			}
+
 		}
+		setPixel(e.x, e.y, g);
 		g.setPaintMode();
 	}
-
 }
