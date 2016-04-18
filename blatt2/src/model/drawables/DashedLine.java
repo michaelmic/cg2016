3e8ce59a45b75904c058e7d822fd82f3d54b741e
michaelmic <michmicke@gmail.com>
2016-04-19 00:39:13 +0200

    Dashed

--- blatt2/src/model/drawables/DashedLine.java
+++ blatt2/src/model/drawables/DashedLine.java
@@ -3,6 +3,8 @@
  */
 package model.drawables;
 
+import java.awt.Graphics;
+
 /**
  * Eine DashedLine ist eine Linie, die im XOR-Modus gestrichelt gezeichnet wird.
  * 
@@ -11,14 +13,39 @@ package model.drawables;
  */
 // TODO: (A3) Klasse vervollständigen
 public class DashedLine extends Line {
+	// Abstand zwischen den gestrichelten Linien
+	private final int DISTANCE = 10;
+	private int dashCount;
+	// falls true wird gezeichnet, sonst nicht
+	private boolean toggleDraw;
+	
 	/**
 	 * @see Line
 	 */
 	public DashedLine(Point a, Point e) {
 		super(a, e);
+		
+		dashCount = 0;
+		toggleDraw = true;
 	}
 
 	// TODO: (A3) Methoden überschreiben, damit die Linie
 	// gestrichelt und im XOR-Modus gezeichnet wird
+	@Override
+	protected void setPixel(int x, int y, Graphics g) {
+		
+		// Zaehler erhoehen
+		dashCount++;
+		// ggf toggleDraw invertieren (sind bereits 10 Pixel gesetzt worden?)
+		if (dashCount >= DISTANCE) {
+			toggleDraw = !toggleDraw;
+			dashCount = 0;
+		}
+		
+		// der Stift ist auf dem Papier abgesetzt falls toggleDraw == true
+		if(toggleDraw) {
+			super.setPixel(x, y, g);
+		}
+	}
 
 }
--- blatt2/src/model/drawables/DashedRectangle.java
+++ blatt2/src/model/drawables/DashedRectangle.java
@@ -3,6 +3,8 @@
  */
 package model.drawables;
 
+import java.awt.Graphics;
+
 /**
  * Repräsentiert ein Rectangle, das gestrichelt und im XOR-Modus gezeichnet
  * wird.
@@ -17,5 +19,57 @@ public class DashedRectangle extends Rectangle {
 	 */
 	public DashedRectangle(Point a, Point e) {
 		super(a, e);
+	}
+	
+	@Override
+	public void paint(Graphics g) {
+		Point p = new Point(this.a);
+		int dist = 10;
+		
+		// Horizontale Linie zeichnen
+		if( this.a.x < this.e.x ) {
+			while(p.x + dist < this.e.x) {
+				g.drawLine(p.x, p.y, p.x + dist, p.y);
+				g.drawLine(p.x, this.e.y, p.x + dist, this.e.y);
+				p.x += 2*dist;
+			}
+			if(p.x < this.e.x) {
+				g.drawLine(p.x, p.y, this.e.x, p.y);
+				g.drawLine(p.x, this.e.y, this.e.x, this.e.y);
+			}
+		} else {
+			while(p.x - dist > this.e.x) {	
+				g.drawLine(p.x, p.y, p.x - dist, p.y);
+				g.drawLine(p.x, this.e.y, p.x - dist, this.e.y);
+				p.x -= 2*dist;
+			}
+			if(p.x > this.e.x) {
+				g.drawLine(p.x, p.y, this.e.x, p.y);
+				g.drawLine(p.x, this.e.y, this.e.x, this.e.y);
+			}
+		}
+		//Vertikale zeichnen
+		p = new Point(this.a);
+		if( this.a.y < this.e.y ){
+			while(p.y + dist < this.e.y) {
+				g.drawLine(p.x, p.y, p.x, p.y + dist);
+				g.drawLine(this.e.x, p.y, this.e.x, p.y + dist);
+				p.y += 2*dist;
+			}
+			if(p.y < this.e.y) {
+				g.drawLine(p.x, p.y, p.x, this.e.y);
+				g.drawLine(this.e.x, p.y, this.e.x, this.e.y);
+			}
+		} else {
+			while(p.y - dist > this.e.y) {
+				g.drawLine(p.x, p.y, p.x, p.y - dist);
+				g.drawLine(this.e.x, p.y, this.e.x, p.y - dist);
+				p.y -= 2*dist;
+			}
+			if(p.y > this.e.y) {
+				g.drawLine(p.x, p.y, p.x, this.e.y);
+				g.drawLine(this.e.x, p.y, this.e.x, this.e.y);
+			}
+		}
 	}
 }
